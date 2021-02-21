dc0a6deae112af5fd18c036841ff99189390a7f2
GabrielPDB <39529079+GabrielPDB@users.noreply.github.com>
2021-02-21 09:35:07 -0300

    first commit

---
+++ instructors.js
@@ -0,0 +1,61 @@
+const { RSA_NO_PADDING } = require('constants')
+const fs = require('fs')
+const data = require('./data.json')
+
+// show
+exports.show = function (req, res) {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        age: '',
+        services: foundInstructor.services.split(','), 
+        created_at: ''
+    }
+
+    return res.render('instructors/show', { instructor })
+}
+
+// create
+exports.post = function (req, res) {
+    // Estratégia de validação de preenchimento de todos os campos
+    const keys = Object.keys(req.body)
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please fill all fields!')
+        }
+    }
+
+    let { avatar_url, birth, name, services, gender } = req.body
+
+    birth = Date.parse(birth) // Transforma uma data em milissegundos
+    const created_at = Date.now() // Pega o tempo atual
+    const id = Number(data.instructors.length + 1) // Força que isso retorne um número
+
+
+    // Adiciona o req.body no array data lá do JSON
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    // Escreve o arquivo, passando o conteúdo de data, com 4 espaços
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), function (err) {
+        if (err) return res.send('Write file error!')
+
+        return res.redirect('/instructors')
+    })
+
+    /* return res.send(req.body) */
+}
